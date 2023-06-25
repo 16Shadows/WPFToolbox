296cb1b5b8bfc386c3774b7373a1224374319101
16Shadows <egor.0.00@mail.ru>
2023-06-26 02:54:54 +0500

    Added special implementation of DeferredCommand which makes use of CommandManager.RequerySuggested.

---
+++ Command/DeferredCommandWPF.cs
@@ -0,0 +1,45 @@
+﻿using CSToolbox.Weak;
+using MVVMToolbox.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFToolbox.Command
+{
+	public class DeferredCommandWPF : DeferredCommand
+	{
+		public DeferredCommandWPF(WeakAction.CallType execute, WeakDelegate<bool>.CallType? canExecute = null) : this(new WeakAction(execute), canExecute != null ? new WeakDelegate<bool>(canExecute) : null)
+		{
+		}
+
+		public DeferredCommandWPF(WeakAction execute, WeakDelegate<bool>? canExecute = null) : base(execute, canExecute)
+        {
+			CommandManager.RequerySuggested += CommandManager_RequerySuggested;
+        }
+
+		private void CommandManager_RequerySuggested(object? sender, EventArgs e)
+		{
+			InvokeCanExecuteChanged();
+		}
+	}
+
+	public class DeferredCommandWPF<T> : DeferredCommand<T>
+	{
+		public DeferredCommandWPF(WeakAction<T?>.CallType execute, WeakDelegate<bool, T?>.CallType? canExecute = null) : this(new WeakAction<T?>(execute), canExecute != null ? new WeakDelegate<bool, T?>(canExecute) : null)
+		{
+		}
+
+		public DeferredCommandWPF(WeakAction<T?> execute, WeakDelegate<bool, T?>? canExecute = null) : base(execute, canExecute)
+        {
+			CommandManager.RequerySuggested += CommandManager_RequerySuggested;
+        }
+
+		private void CommandManager_RequerySuggested(object? sender, EventArgs e)
+		{
+			InvokeCanExecuteChanged();
+		}
+	}
+}
